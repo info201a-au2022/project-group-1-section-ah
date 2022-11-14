8f2b619471c970161d0b2053aa77026813cdd5d5
KaiAndreic <114948216+KaiAndreic@users.noreply.github.com>
2022-11-14 14:20:03 -0800

    added second map

--- source/map_chart.R
+++ source/map_chart.R
@@ -9,26 +9,31 @@ mapdatas <- map_data("world")
 colnames(mapdatas)[5] <- "Country"
 mapdatas <- left_join(mapdatas , all_info, by= "Country")
 
-test <- mapdatas %>% 
+#population
+population1 <- mapdatas %>% 
   select(long, lat, group, Country, order, subregion, `Population size in millions`,Year) %>% 
   filter(Year == 2021)
-tester<- test %>% 
+population2<- population1 %>% 
   select(long, lat, group, Country, order, subregion, `Population size in millions`)
 
-mapdata1s<-tester %>% 
-  filter(!is.na(test$`Population size in millions`)) 
+mapdata1s<-population2 %>% 
+  filter(!is.na(population1$`Population size in millions`)) 
   
-  
-View(mapdata1s)
-
-  
-
-
 map1s<-ggplot(mapdata1s, aes( x = long, y = lat, group=group)) +
-    geom_polygon(aes(fill = `Population size in millions`), color = "black")
+  geom_polygon(aes(fill = `Population size in millions`), color = "black")
 map1s
 
+#Happy
+happiness1 <- mapdatas %>% 
+  select(long, lat, group, Country, order, subregion, `Happiness Index 0 (unhappy) - 10 (happy)`,Year) %>% 
+  filter(Year == 2021)
+happiness2 <- happiness1 %>% 
+  select(long, lat, group, Country, order, subregion, `Happiness Index 0 (unhappy) - 10 (happy)`)
+mapdata2 <- happiness2 %>% 
+  filter(!is.na(happiness1$`Happiness Index 0 (unhappy) - 10 (happy)`))
+happy_map <-ggplot(happiness1, aes( x = long, y = lat, group=group)) +
+  geom_polygon(aes(fill = `Happiness Index 0 (unhappy) - 10 (happy)`), color = "white")
+happy_map
 
-install.packages("cowplot")
-library(cowplot)
+
 
