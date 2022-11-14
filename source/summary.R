b755bd906600171324284244e5fa3863bd586267
yl0517 <yl0517@uw.edu>
2022-11-13 22:11:39 -0800

    3 out of 5

--- source/summary.R
+++ source/summary.R
@@ -3,5 +3,19 @@
 library(dplyr)
 library(tidyverse)
 
-populationData <- read.csv(file = 'data/populationDataset.csv')
-?read.csv()
+populationData <- read.csv("/Users/yoobinlee/Autumn2022/info201/project/project-group-1-section-ah/data/populationDataset.csv")
+
+summary_info <- list()
+summary_info$num_countries <- length(unique(populationData$Country))
+summary_info$highest_population_2021 <- populationData %>%
+  filter(Year == "2021", na.rm = TRUE) %>% 
+  filter(Population.size.in.millions == max(Population.size.in.millions, na.rm = TRUE)) %>%
+  select(Population.size.in.millions, Country)
+summary_info$lowest_population_2021 <- populationData %>% 
+  filter(Year == "2021", na.rm = TRUE) %>% 
+  filter(Population.size.in.millions == min(Population.size.in.millions, na.rm = TRUE)) %>%
+  select(Population.size.in.millions, Country)
+
+
+
+View(summary_info)
