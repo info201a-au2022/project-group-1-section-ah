85f91c3c8eeb97a2e50d4e7b4842055acab6bfb1
yl0517 <yl0517@uw.edu>
2022-11-29 18:15:28 -0800

    1

--- source/chart1.R
+++ source/chart1.R
@@ -31,10 +31,6 @@ ui <- fluidPage(
 )
 
 
-
-
-
-
 population_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-ah/main/data/populationDataset.csv") %>% 
   data.frame()
 View(population_data)
