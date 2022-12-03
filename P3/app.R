150b097a98d3def8b895a412507ee30c56fcce2c
Tom Sander <thomasjay256@gmail.com>
2022-12-02 16:51:02 -0800

    perms

--- P3/app.R
+++ P3/app.R
@@ -21,5 +21,7 @@ rsconnect::setAccountInfo(name='kaiandreic',
                           token='1D18FCC92F117605869484F4DB34910E',
                           secret='Z7dWEkgGy2Roej/uyUKE4bpAdXnBIiNdRzhsRuIV')
 
-
+rsconnect::setAccountInfo(name='tjsander',
+                          token='A8ADE5E1D16AA937582A152A423A1E50',
+                          secret='C4aQ2loBvYedGy9YFF6vD/WpmDudz0V68gt/azUV')
 #https://kaiandreic.shinyapps.io/AH-1-P3/
